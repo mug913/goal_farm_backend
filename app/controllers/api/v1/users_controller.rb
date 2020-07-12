8479f948a5ab1ef578ec24c8f0e21bf7454b08f2
Mug <brad.alexander913@gmail.com>
2020-07-11 18:31:45 -0700

    added user show route

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,18 +1,23 @@
 class Api::V1::UsersController < ApplicationController
 
     def index 
-    users = User.all
-    render json: UserSerializer.new(users).to_serialized_json
+        users = User.all
+        render json: UserSerializer.new(users).all_to_serialized_json
+    end
+
+    def show
+        user = User.find_by_id(params[:id])
+        render json: UserSerializer.new(user).to_serialized_json
+
     end
 
     def create
         user = User.new(user_params)
-            if user.save
-                render json: user, status: :accepted
-            else
-                render json: {erros: goal.errors.full_messages}, status:
-                :unprocessible_entity
-            end
+        if user.save
+            render json: user, status: :accepted
+        else
+            render json: {erros: goal.errors.full_messages}, status:
+            :unprocessible_entity
         end
     end
 
--- app/services/user_serializer.rb
+++ app/services/user_serializer.rb
@@ -3,9 +3,14 @@ class UserSerializer
         @user = user
     end
 
+    def all_to_serialized_json
+        @user.to_json(:only => [:id, :username])
+    end
+
     def to_serialized_json
         @user.to_json(:include => {
             :goals => {:only => [:target, :id]}
         }, :except => [:password_digest])
     end
+ 
 end
