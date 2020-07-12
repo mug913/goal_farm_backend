ac1f2ddc189fe82d4d941e24c72c653402edfa4e
Mug <brad.alexander913@gmail.com>
2020-07-12 11:05:29 -0700

    enabled cors

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,13 @@ Bundler.require(*Rails.groups)
 
 module GoalFarmBackend
   class Application < Rails::Application
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
@@ -33,5 +40,7 @@ module GoalFarmBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    
   end
 end
